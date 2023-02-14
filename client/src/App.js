3f65a6f5863c0a2558073d9c25de66026bbb5b03
troubles <cesar.troubles@gmail.com>
2023-02-13 20:55:06 -0500

    detalles al navBar

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { Home } from "./views/index.js";
 function App() {
   return (
     <div className="App">
-        <Route exact path={"/home"} render={()=> <Home/>}/>
+        <Route exact path={"/"} render={()=> <Home/>}/>
     </div>
   );
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,16 +2,17 @@ import style from "./NavBar.module.css"
 import logo_compuShop from "../../assets/compu-shop_logo.png"
 import shoppingCart from "../../assets/icons/black-shopping-cart.svg"
 import SearchBar from "../SearchBar/SearchBar";
+import loggingImg from "../../assets/icons/logging.svg"
 
 
 const NavBar = ()=>{
     return(
         <div id={style.NavBarContainer}>
             <div id={style.NavBar}>
-                <div><img src={logo_compuShop} alt="" /></div>
+                <div id={style.logoContainer}><img src={logo_compuShop} alt="logo_compuShop" /></div>
                 <SearchBar/>
                 <div id={style.shoppingCart}><img src={shoppingCart} alt="shoping Cart"/></div>
-                <button id={style.logging}>INICIAR SESIÓN</button> 
+                <button id={style.logging}> <img src={loggingImg} alt="logging" /><span>INICIAR SESIÓN</span></button> 
             </div>
         </div>
     )
