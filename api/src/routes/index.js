1c94ba9024c526c4e89fc8db76b4ca72d70f0719
gordoenredadera <naviilleib@gmail.com>
2023-02-13 22:33:26 -0300

    Made a faker to work with while we figure out the data we'll use

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
+const {allComps} = require("../controllers/components.js")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
 const router = Router();
+
+router.get("/components", async (req, res) =>{
+    try {
+        return res.status(200).send(await allComps())
+    } catch (error) {
+        return error
+    }
+})
+
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
