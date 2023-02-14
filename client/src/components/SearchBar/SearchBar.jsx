99a9420ddebc8dc10cbd0d1ef0211e33f047e6f3
troubles <cesar.troubles@gmail.com>
2023-02-14 14:50:18 -0500

    Darle un poco de css al HEADER y agregarle controladores de estado

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,36 @@
 import style from "./SearchBar.module.css"
+import { useState } from "react";
 import lupa from "../../assets/icons/lupa.png"
+import xIcon from "../../assets/icons/X-icon_black.svg"
+
+
+
 
 const SearchBar = () =>{
+
+    const [inputStatus,setInputStatus] = useState('') 
+    const [logoSearchBarStatus, setLogoSearchBarStatus] = useState(true)
+
+    const hanlderChange=(e)=>{
+        const value = e.target.value
+        setInputStatus(value)
+        value.length > 0 ? setLogoSearchBarStatus(false): setLogoSearchBarStatus(true)
+    }
+
+    const handlerSubmit=(e) =>{
+        e.preventDefault();
+        alert(`estás buscando <<${inputStatus}>>`)
+    }
+
     return(
-        <div id={style.SearchBarContainer}><input placeholder="Buscar componente" type="text" /><button><img src={lupa} alt="lupa" /></button></div>
+        <div id={style.SearchBarContainer}  >
+            <form action="" onSubmit={e=>handlerSubmit(e)}  >
+                  <input placeholder="Buscar componente" type="text" value={inputStatus} onChange={hanlderChange}  />
+            </form>
+            {logoSearchBarStatus?
+                                <button onClick={()=>{setInputStatus("")}  } ><img src={lupa} alt="lupa" /></button>   :
+                                <button id={style.xIcon} onClick={()=>{setInputStatus(""); setLogoSearchBarStatus(!logoSearchBarStatus)}} ><img src={xIcon} alt="X icon" /></button>}
+        </div>
     )
 };
 
