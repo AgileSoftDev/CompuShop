945664d38e5ba103db22f121f8533142af470aa4
Mateo Carlos <mateocarlos402@hotmail.com>
2023-02-15 12:37:47 -0300

    Principio order

---
+++ client/src/components/Order/Order.jsx
@@ -0,0 +1,17 @@
+import style from "./Order.module.css"
+
+
+const Order = ()=> {
+    return(
+        <div className={style.container}>
+            <select className={style.select}>
+                <option>Ordenar por</option>
+                <option>Destacados</option>
+                <option>Mayor precio</option>
+                <option>Menor precio</option>
+            </select>
+        </div>
+    )
+}
+
+export default Order;
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import {style} from './Home.module.css';
 import Header from '../../components/Header/Header';
 import SideBar from '../../components/SideBar/SideBar';
+import Order from '../../components/Order/Order';
 
 
 const Home = ()=>{
     return(
         <div>
             <Header/>
+            <Order/>
             <SideBar/>
         </div>
     )
