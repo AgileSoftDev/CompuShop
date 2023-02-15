3a6cf139954de3ad6fbdc21160a2c6277b87dc6e
troubles <cesar.troubles@gmail.com>
2023-02-15 11:01:56 -0500

    "Cambio de ruta productos"

--- client/src/views/Productos/Productos.jsx
+++ client/src/views/Productos/Productos.jsx
@@ -1,10 +1,12 @@
 import {style} from './Productos.module.css';
 import SideBar from '../../components/SideBar/SideBar';
+import Order from "../../components/Order/Order.jsx"
 
 
 const Home = ()=>{
     return(
         <div>
+            <Order/>
             <SideBar/>
         </div>
     )
