38beaf7bb915ce6b3c16135d0956c6c088433768
troubles <cesar.troubles@gmail.com>
2023-02-16 18:45:29 -0500

    vista lading ruta actualizada

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,20 @@ import Header from './components/Header/Header.jsx';
 import Productos from './views/Productos/Productos.jsx'
 import Footer from "./components/Footer/Footer";
 import LandingPage from "../src/components/LandingPage/LandingPage.jsx"
+import { useLocation } from "react-router-dom";
 
 
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
-        <Header/>
+        { location.pathname!=='/' && <Header/>}
         <Route exact path={"/"} render={()=> <LandingPage/>}/>
         <Route exact path={"/home"} render={()=> <Home/>}/>
         <Route exact path={"/productos"} render={()=> <Productos/>} />
-        <Footer/>
+        { location.pathname !=='/' && <Footer/>}
     </div>
   );
 }
