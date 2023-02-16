c6e59a7574b8295c69d88e4324664ec70e2a7df7
Bruno Fernandez <66654095+brunofnz@users.noreply.github.com>
2023-02-16 12:52:08 -0300

    ADD product card y slider

--- client/src/components/ProductCard/ProducCard.jsx
+++ client/src/components/ProductCard/ProducCard.jsx
@@ -2,21 +2,12 @@ import React from "react";
 import { Card, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ProductCard = props => {
-     let { imgSrc, price, title} = props.data;
-
+const ProductCard = ({ imgSrc }) => {
    return (
     < Card className="p-0 overflow-hidden h-100 shadow"> 
         <div className=" overflow-hidden roudend p-0 bg-light">
             <Card.Img variant="top" src={imgSrc} /> 
         </div>
-        <Card.Body className="text-center">
-        
-           <Card.Title className="display-6">{price}</Card.Title>
-           <Card.Title >{title} </Card.Title>
-        </Card.Body>
-           <Button className="w-100 rounded-0" variant="success">Add to Card</Button>
-        
     </Card>
    );
 };
