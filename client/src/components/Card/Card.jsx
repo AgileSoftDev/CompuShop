d9ca133aa18c134f7f2da86a7226c6fb634cdaf3
troubles <cesar.troubles@gmail.com>
2023-02-15 20:22:40 -0500

    vista horizontal de las cards DONE

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -10,21 +10,38 @@ const Card = ({ id, title, image, precio }) => {
     const stateViewCard = useSelector(e=>e.stateViewCard)
 
 
-    return (
-      <Link to={`/producto/${id}`} id={ stateViewCard ? style.Card : style.Card2}>
-            <div>
-                <div >
-                     <img  src={image} alt={title} />
-                </div>
-                <h1 >{title}</h1>
-            </div>
-            <div>
-                <h1 >$ {precio}</h1>
-                <button>SUMAR AL CARRITO</button>
-            </div>
-
-      </Link>
-    );
+    if(stateViewCard){
+        return (
+            <Link to={`/producto/${id}`} id={style.Card }>
+                  <div>
+                      <div >
+                           <img  src={image} alt={title} />
+                      </div>
+                      <h1 >{title}</h1>
+                  </div>
+                  <div>
+                      <h1 >$ {precio}</h1>
+                      <button>SUMAR AL CARRITO</button>
+                  </div>
+      
+            </Link>
+          );
+    }else{
+        return (
+            <Link to={`/producto/${id}`} id={style.Card2 }>
+                  <div>
+                           <img  src={image} alt={title} />
+                  </div>
+                  <div>
+                      <h2 >{title}</h2>
+                      <h1 >$ {precio}</h1>
+                      <button>SUMAR AL CARRITO</button>
+                  </div>
+      
+            </Link>
+          );
+    }
+   
 };
 
 export default Card
