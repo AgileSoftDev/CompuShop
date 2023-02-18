2c8b471a6fc79fe771bb12f5f883519bbb002648
troubles <cesar.troubles@gmail.com>
2023-02-17 19:15:18 -0500

    corrección import

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import style from "./App.module.css";
-import {Route, Router} from "react-router-dom";
+import {Route} from "react-router-dom";
 import { Home } from "./views/index.js";
 import Header from './components/Header/Header.jsx';
 import Productos from './views/Productos/Productos.jsx'
 import Footer from "./components/Footer/Footer";
 import LandingPage from "../src/components/LandingPage/LandingPage.jsx"
 import { useLocation } from "react-router-dom";
-<<<<<<< HEAD
 import Construye from "./views/Construye/Construye";
-=======
 import ProfileDetail from "./views/ProfileDetail/ProfileDetail";
->>>>>>> de9b5169ca74ffc6540c612923f24cfb8499fed9
 
 
 
@@ -20,19 +17,13 @@ function App() {
   return (
     <div id={style.AbsoluteContaier}>
         { location.pathname!=='/' && <Header/>}
-<<<<<<< HEAD
         <div id={style.AbsoluteContaier_Main}>
               <Route exact path={"/"} render={()=> <LandingPage/>}/>
               <Route exact path={"/home"} render={()=> <Home/>}/>
               <Route exact path={"/productos"} render={()=> <Productos/>} />
+              <Route exact path={"/profile"} render={()=> <ProfileDetail/>}/>
               <Route  path={"/construye"} render={()=><Construye/>}/>
         </div>
-=======
-        <Route exact path={"/"} render={()=> <LandingPage/>}/>
-        <Route exact path={"/home"} render={()=> <Home/>}/>
-        <Route exact path={"/productos"} render={()=> <Productos/>} />
-        <Route exact path={"/profile"} render={()=> <ProfileDetail/>}/>
->>>>>>> de9b5169ca74ffc6540c612923f24cfb8499fed9
         { location.pathname !=='/' && <Footer/>}
     </div>
   );
