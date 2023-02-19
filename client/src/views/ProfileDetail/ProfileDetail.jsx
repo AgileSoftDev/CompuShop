9b0199c9a2d041844e7c1c44bcb31d90efd05852
Mateo Carlos <mateocarlos402@hotmail.com>
2023-02-19 15:18:06 -0300

    Update profile detail

--- client/src/views/ProfileDetail/ProfileDetail.jsx
+++ client/src/views/ProfileDetail/ProfileDetail.jsx
@@ -1,47 +1,55 @@
 import React from "react";
 import style from "./ProfileDetail.module.css"
 import { useAuth0 } from "@auth0/auth0-react";
+import {Link} from 'react-router-dom'
 
 
 const ProfileDetail = () => {
 
-    const { user } = useAuth0();
+    const { user, logout } = useAuth0();
 
     return (
-        <div className={style.container}>
-            <div className={style.cuadro}>
+        <div className={style.fondo}>
+            <div className={style.container}>
+                <p className={style.titulo}><i class="fa-solid fa-user "></i> Mi cuenta</p>
+                <Link to={"/productos"}><p className={style.volver}>🡸</p></Link>
+                
+                <div className={style.izquierda}>
+                    <div className={style.cuadro1}>
+                        <h2><i class="fa-solid fa-user-secret"></i> Mi información</h2>
+                        <p>Nombre: {user.name}</p>
+                        <p>Email: {user.email}</p>
+                        <p>Celular: {user.phone_number}</p>
+                    </div>
 
-                <div className={style.info}>
-                    <img className={style.img} src={user.picture} alt={user.name}/>
-                    <h1>Hola {user.name}!</h1>
+                    <div className={style.cuadro2}>
+                        <h2><i class="fa-solid fa-bag-shopping"></i> Mis compras</h2>
+                        <p>Aqui van a aparecer tus futuras reviews de productos</p>
+                    </div>
+
+                    <div className={style.cuadro3}>
+                        <h2><i class="fa-solid fa-star"></i> Mis reviews</h2>
+                        <p>Aqui van a aparecer tus futuras compras</p>
+                    </div>
                 </div>
 
-                <div className={style.info2}>
-                    <h2>Tu informacion:</h2>
-                    <div className={style.email}>
-                        <p>Email:</p>
-                        <p>{user.email}</p>
+                <div className={style.derecha}>
+                    <div className={style.cuadro4}>
+                        <h2><i class="fa-solid fa-file-invoice-dollar"></i> Mis facturas</h2>
+                        <p>Aqui van a aparecer tus futuras facturas</p>
                     </div>
 
-                    <div className={style.ciudad}>
-                        <p>Ciudad:</p>
-                        <p>{user.name}</p>
+                    <div className={style.cuadro5}>
+                        <h2><i class="fa-solid fa-map-location-dot"></i> Mi dirección</h2>
+                        <p>Direccion actual: {user.address}</p>
+                        <button className={style.direccion}>Actualizar mi dirección</button>
                     </div>
 
-                    <div className={style.direccion}>
-                        <p>Direccion:</p>
-                        <p>{user.email}</p>
+                    <div className={style.cuadro6}>
+                        <Link to={"/shoppingCart"}><button className={style.carrito}><i class="fa-solid fa-cart-shopping"></i> Carrito</button></Link>
+                        <button className={style.datos}><i class="fa-solid fa-wrench"></i> Actualizar datos</button>
+                        <button className={style.salir} onClick={()=> logout({ returnTo: window.location.origin})}><i class="fa-solid fa-right-from-bracket"></i> Cerrar sesión</button>
                     </div>
-
-                    <div className={style.celular}>
-                        <p>Celular:</p>
-                        <p>{user.name}</p>
-                    </div>
-
-                </div>
-
-                <div>
-                    
                 </div>
             </div>
         </div>
