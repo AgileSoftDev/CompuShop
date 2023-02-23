ecb0deccec4c1bbc76375cd1200640b338543cf5
kan4crooo <kevinacr17@gmail.com>
2023-02-23 07:46:01 -0500

    model user added

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -1,32 +1,32 @@
-// const { Schema, model }= require("mongoose");
+const { Schema, model }= require("mongoose");
 
-// const userSchema= new Schema(
-//     {
-//         userName:{
-//             type: String,
-//             required: true,
-//         },
-//         userCategory:{
-//             type: String,
-//             required: true,
-//         },
-//         email:{
-//             type: String,
-//             required: true,
-//         },
-//         password:{
-//             type: String,
-//             required: true,
-//         },
-//         wallet:{
-//             type: String,
-//             required: true,
-//         },
-//         phoneNumber:{
-//             type: Number,
-//         }
-// });
+const userSchema= new Schema(
+    {
+        userName:{
+            type: String,
+            required: true,
+        },
+        userCategory:{
+            type: String,
+            required: true,
+        },
+        email:{
+            type: String,
+            required: true,
+        },
+        password:{
+            type: String,
+            required: true,
+        },
+        wallet:{
+            type: String,
+            required: true,
+        },
+        phoneNumber:{
+            type: Number,
+        }
+});
 
-// const User= model("User", userSchema) 
+const User= model("User", userSchema) 
 
-// module.exports= User
+module.exports= User
