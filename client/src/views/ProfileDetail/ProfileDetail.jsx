791043a9ea640c052cc969d0ba0ff2f07f98b528
yorliana <38323669+yorliana@users.noreply.github.com>
2023-03-06 14:49:21 -0500

    df

--- client/src/views/ProfileDetail/ProfileDetail.jsx
+++ client/src/views/ProfileDetail/ProfileDetail.jsx
@@ -32,7 +32,7 @@ const ProfileDetail = () => {
                         <div className={style.cuadro2}>
                             <h2><i class="fa-solid fa-bag-shopping"></i> Mis compras</h2>
                             <p>Aqui van a aparecer tus futuras reviews de productos</p>
-                            <Link to={"/compra"} ><button className={style.datos}><i class="fa-solid fa-bag-shopping"></i> Mis Compras</button></Link>
+                            <Link to={"/compra"} ><button className={style.compra}><i class="fa-solid fa-bag-shopping"></i> Mis Compras</button></Link>
                         </div>
                         
                         <div className={style.cuadro3}>
